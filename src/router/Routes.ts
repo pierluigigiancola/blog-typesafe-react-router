5a8b5d76550c0b8b924e527d403b73e1fca64e20
pierluigigiancola <pierluigi.giancola@gmail.com>
2023-02-26 17:43:54 +0100

    feat: params inference based on to props

--- src/router/Routes.ts
+++ src/router/Routes.ts
@@ -1,11 +1,26 @@
-
 const Routes = {
-    RGB: '/rgb/:red/:green/:blue',
-    HSL: '/hsl/:hue/:saturation/:lightness',
+  RGB: "/rgb/:red/:green/:blue",
+  HSL: "/hsl/:hue/:saturation/:lightness",
 } as const;
 
-export type RouteLabels = keyof typeof Routes;
+export type TRoutes = typeof Routes;
+
+export type RouteLabels = keyof TRoutes;
 
 export type RoutePaths = typeof Routes[RouteLabels];
+
+type ParseRouteParams<T extends string> =
+  T extends `${infer _}:${infer Param}/${infer Rest}`
+    ? { [K in Param | keyof ParseRouteParams<Rest>]: string }
+    : T extends `${infer _}:${infer Param}`
+    ? { [K in Param]: string }
+    : {};
+
+export type RoutesProperty = {
+  [K in RouteLabels]: {
+    path: K;
+    params: ParseRouteParams<typeof Routes[K]>;
+  };
+};
 
 export default Routes;
